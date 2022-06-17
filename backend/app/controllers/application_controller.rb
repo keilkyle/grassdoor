42a774773a7d01dda39a2ebdaee5121251323663
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-06-16 22:34:11 -0700

    okay now the backend is done, made the controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,9 +1,54 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/lawns" do
+    lawns = Lawn.all
+    lawns.to_json
+  end
+
+  post "/lawns" do
+    lawn = Lawn.create(
+      name: params[:name],
+      address: params[:address],
+      image_url: params[:image_url]
+    )
+    lawn.to_json
+  end
+
+  get "/reviews" do
+    reviews = Review.all
+    reviews.to_json
+  end
+
+  get "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.to_json
+  end
+
+  post "/reviews" do 
+    review = Review.create(
+      body: params[:body],
+      author: params[:author],
+      lawn_id: params[:lawn_id],
+      review_date: params[:review_date],
+      stars: params[:stars]
+    )
+    review.to_json
+  end
+
+  patch "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.update(
+      body: params[:body],
+      stars: params[:stars]
+    )
+    review.to_json
+  end
+
+  delete "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
   end
 
 end
